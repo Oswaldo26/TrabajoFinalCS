c7afc8c2367a8ede69e48b6381ab97072bed2cb0
jairLucas <jlucasb@unsa.edu.pe>
2021-12-11 23:15:18 -0500

    contador de items del carrito"

--- carts/views.py
+++ carts/views.py
@@ -58,7 +58,6 @@ def cart(request, total=0, quantity=0, cart_items=None):
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
-
         tax = (2*total)/100
         grand_total = total + tax
 
