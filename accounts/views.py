80fc85103c04a1e67a9beb0c4d0a18d5017cd732
Oswaldo26 <ozanca@unsa.edu.pe>
2021-12-13 19:53:33 -0500

    Seccion 31 terminada - Chechout

--- accounts/views.py
+++ accounts/views.py
@@ -10,6 +10,10 @@ from django.contrib.auth.tokens import default_token_generator
 from django.utils.encoding import force_bytes
 from django.core.mail import EmailMessage
 
+from carts.views import _cart_id
+from carts.models import Cart, CartItem
+import requests
+
 # Create your views here.
 def register(request):
     form = RegistrationForm()
@@ -52,9 +56,58 @@ def login(request):
 
         user = auth.authenticate(email=email, password=password)
         if user is not None:
+
+            try:
+                cart = Cart.objects.get(cart_id = _cart_id(request))
+                is_cart_item_exists = CartItem.objects.filter(cart=cart).exists()
+                if is_cart_item_exists:
+                    cart_item = CartItem.objects.filter(cart=cart)
+
+                    product_variation = []
+                    for item in cart_item:
+                        variation = item.variation.all()
+                        product_variation.append(list(variation))
+
+                    cart_item  = CartItem.objects.filter(user=user)
+                    ex_var_list = []
+                    id= []
+                    for item in cart_item:
+                        existing_variation = item.variation.all()
+                        ex_var_list.append(list(existing_variation))
+                        id.append(item.id)
+
+                    for pr in product_variation:
+                            if pr in ex_var_list:
+                                index = ex_var_list.index(pr)
+                                item_id = id[index]
+                                item = CartItem.objects.get(id=item_id)
+                                item.quantity +=1
+                                item.user = user
+                                item.save()
+                            else:
+                                cart_item = CartItem.objects.filter(cart=cart)
+                                for item in cart_item:
+                                    item.user = user
+                                    item.save()
+            except:
+                pass
+
+
+
+
             auth.login(request, user)
-            messages.success(request, 'Haz inicado sesión exitosamente')
-            return redirect('dashboard')
+            messages.success(request, 'Has inicado sesión exitosamente')
+
+            url = request.META.get('HTTP_REFERER')
+            try:
+                query = requests.utils.urlparse(url).query
+                params = dict(x.split('=')for x in query.split('&'))
+                if 'next' in params:
+                    nexPage = params['next']
+                    return redirect(nexPage)
+            except:
+                return redirect('dashboard')
+
         else:
             messages.error(request, 'Las credenciales son incorrectas')
             return redirect('login')
@@ -86,3 +139,65 @@ def activate(request, uidb64, token):
 @login_required(login_url='login')
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')
+
+
+
+def forgotPassword(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        if Account.objects.filter(email=email).exists():
+            user = Account.objects.get(email__exact=email)
+
+            current_site = get_current_site(request)
+            mail_subject = 'Resetear Password'
+            body = render_to_string('accounts/reset_password_email.html',{
+                'user': user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, body, to=[to_email])
+            send_email.send()
+
+            messages.success(request, 'Un email fue enviado a tu bandeja de entrada para resetear tu password')
+            return redirect('login')
+        else:
+            messages.error(request, 'La cuenta de usuario no existe')
+            return redirect('forgotPassword')
+
+    return render(request, 'accounts/forgotPassword.html')
+
+
+def resetpassword_validate(request, uidb64, token):
+        try:
+            uid = urlsafe_base64_decode(uidb64).decode()
+            user = Account._default_manager.get(pk=uid)
+        except(TypeError, ValueError, OverflowError, Acccount.DoesNotExist):
+            user=None
+
+        if user is not None and default_token_generator.check_token(user, token):
+            request.session['uid'] = uid
+            messages.success(request, 'Por favor resetea tu password')
+            return redirect('resetPassword')
+        else:
+            messages.error(request, 'El link ha expirado')
+            return redirect('login')
+
+def resetPassword(request):
+        if request.method == 'POST':
+            password = request.POST['password']
+            confirm_password = request.POST['confirm_password']
+
+            if password == confirm_password:
+                uid = request.session.get('uid')
+                user = Account.objects.get(pk=uid)
+                user.set_password(password)
+                user.save()
+                messages.success(request, 'El password se reseteo correctamente')
+                return redirect('login')
+            else:
+                messages.error(request, 'El password de confirmacion no concuerda')
+                return redirect('resetPassword')
+        else:
+            return render(request, 'accounts/resetPassword.html')
