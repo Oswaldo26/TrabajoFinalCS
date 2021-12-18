605c53976260fbc561add319ecffa0966451620c
jairLucas <jlucasb@unsa.edu.pe>
2021-12-18 03:54:33 -0500

    agregando estilo a los comentarios"

--- accounts/views.py
+++ accounts/views.py
@@ -12,7 +12,7 @@ from django.core.mail import EmailMessage
 
 from carts.views import _cart_id
 from carts.models import Cart, CartItem
-import requests
+
 
 # Create your views here.
 def register(request):
