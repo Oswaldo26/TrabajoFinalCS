416e615cc8577a216c9115203a2ec404762a9631
ind-agr <iagramontem@unsa.edu.pe>
2021-12-19 00:52:11 -0500

    Panel de mis ordenes de compra

--- accounts/urls.py
+++ accounts/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path('resetpassword_validate/<uidb64>/<token>/', views.resetpassword_validate, name='resetpassword_validate'),
     path('resetPassword/', views.resetPassword, name='resetPassword'),
     path('activate/<uidb64>/<token>/', views.activate, name='activate'),
+    path('my_orders/', views.my_orders, name='my_orders'),
 ]
